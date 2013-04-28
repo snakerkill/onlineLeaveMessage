352e057fc036189798fa07c5ee60ba153ebd3f08
snakerkill <snakerkill@163.com>
2013-04-29 00:43:38 +0800

    Update createMessageTable.sql

--- createMessageTable.sql
+++ createMessageTable.sql
@@ -3,6 +3,6 @@ create table if not exists message (
 		message_content text  comment '留言内容',
 		user_id int(11) not null comment '用户id',
 		message_time datetime comment '创建时间',
-		is_answer bool not null default 0 comment '是否是回复信息',
+		answer_id int(11) not null default -1 comment '回复信息id,没有回复时为-1',
 		index user_id (user_id)
 		);
