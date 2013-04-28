327aa97fe20f5a9b9777984ec12e822eb2c65be0
snakerkill <snakerkill@163.com>
2013-04-29 01:07:41 +0800

    Update createUserTable.sql

--- createUserTable.sql
+++ createUserTable.sql
@@ -1,13 +1,12 @@
-drop table user_information;
 create table if not exists user_information (
 		user_id int(11) unsigned not null auto_increment primary key comment '用户id',
 		username varchar(20) not null comment '用户名',
 		password varchar(20) not null comment '密码',
 		leave_authority bool not null default 0 comment '留言权限',
 		view_authority bool not null default 0 comment '查看权限',
-		manage_authority bool not null default 0 comment '管理员权限',
-		superuser bool not null default 0 comment '超级用户标志',
-		create_time datetime comment '创建时间',
-		index username (username)
+		user_type bool not null default 0 comment '0普通 1管理 2超级 -1冻结',
+		create_time int(11) comment '创建时间',
+		phone int(11) comment '手机号',
+		index username (username),
+		index phone (phone),
 		);
-insert into user_information(username,password,leave_authority,view_authority,manage_authority,superuser,create_time) values('superuser','test1314',1,1,1,1,now());
